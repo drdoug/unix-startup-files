a62f4937eb282332e88b89e3c0643e3c27a45346
drdoug <drdoug@mimeticsystems.com>
2024-10-31 20:25:17 -0700

    Added option to overwrite master copies with local changes (-X option)

--- update-startup-files.sh
+++ update-startup-files.sh
@@ -8,14 +8,27 @@ function updateFile
         if [[ $_RET != 0 ]];
         then
                 _OUTOFDATE=1
-                printf "%-50.50s needs to be updated from %s\n" "$2" "$1"
+
+                if [[ $_INSTALL_COPIES == 1 && $_FORCE_UPDATES == 1 ]];
+                then
+                        printf "Only ONE of the -F or -X option can be used.\nCannot continue.\n"
+
+                        #
+                        # this will exit
+                        #
+                        _help
+                fi
+
+                printf "%-50.50s is different from %s\n" "$2" "$1"
+
                 if [[ $_FIND_DIFFS == 1 ]];
                 then
                         printf "\nDIFFERENCES between %s and %s:\n" "$1" "$2"
                         diff "$1" "$2"
                         printf "\n"
                 fi
-                if [[ $_FORCE_COPY == 1 ]];
+
+                if [[ $_INSTALL_COPIES == 1 ]];
                 then
                         printf "Copy %s to %s? [y or n]: " "$1" "$2"
                         read _COPYABLE
@@ -24,6 +37,38 @@ function updateFile
                                 cp "$1" "$2"
                         else
                                 printf "     %s was NOT copied to %s\n" "$1" "$2"
+                        fi
+                fi
+
+                if [[ $_FORCE_UPDATES == 1 ]];
+                then
+                        if [[ $_WARNED_ONCE == 0 ]];
+                        then
+                                printf " +-------------------------------------------+\n"
+                                printf ' | mmmm     mm   mm   m   mmm  mmmmmm mmmmm  |\n'
+                                printf ' | #   "m   ##   #"m  # m"   " #      #   "# |\n'
+                                printf ' | #    #  #  #  # #m # #   mm #mmmmm #mmmm" |\n'
+                                printf ' | #    #  #mm#  #  # # #    # #      #   "m |\n'
+                                printf ' | #mmm"  #    # #   ##  "mmm" #mmmmm #    " |\n'
+                                printf " +-------------------------------------------+\n"
+                                printf " |   This option will OVERWRITE the MASTER   |\n"
+                                printf " |   files with any locally installed files  |\n"
+                                printf " |   that are different.  This should ONLY   |\n"
+                                printf " |   be done AFTER changing local files and  |\n"
+                                printf " |    TESTING that the changes are CORRECT   |\n"
+                                printf " |    and WORTHY of BEING SAVED as MASTERS   |\n"
+                                printf " +-------------------------------------------+\n"
+                                _WARNED_ONCE=1
+                        fi
+
+                        printf "OVERWRITE %s WITH %s? [y or n]: " "$1" "$2"
+                        read _COPYABLE
+                        if [[ $_COPYABLE == 'y' ]];
+                        then
+                                cp "$2" "$1"
+                                printf "     %s was OVERWRITTEN with %s\n" "$1" "$2"
+                        else
+                                printf "     %s was NOT OVERWRITTEN with %s\n" "$1" "$2"
                         fi
                 fi
         fi
@@ -51,24 +96,32 @@ function _help
         printf "       The -d flag will print differences between these files and what is installed\n"
         printf "       The -F flag will ask if you wish to update out-of-date files, otherwise only a check is performed\n"
         printf "       The -C flag will ask if you wish to create missing configuration directories\n"
+        printf "       The -X flag will ask if you wish to OVERWRITE these MASTER FILES with THOSE INSTALLED WITH THE CURRENT LOGIN.\n"
+        printf "              WARNING: this is ONLY used when you need to change the default file contents.\n"
         exit 1
         }
 
+_WARNED_ONCE=0
+
 _OUTOFDATE=0
 _FIND_DIFFS=0
-_FORCE_COPY=0
+_FORCE_UPDATES=0
+_INSTALL_COPIES=0
 
-while getopts "dFC" opt; do
+while getopts "dFCX" opt; do
         case ${opt} in
         d )
                 _FIND_DIFFS=1
                 ;;
         F )
-                _FORCE_COPY=1
+                _INSTALL_COPIES=1
                 ;;
         C )
                 createNeededDirs ~/.config/shells
                 createNeededDirs ~/.vim/colors
+                ;;
+        X )
+                _FORCE_UPDATES=1
                 ;;
         \? )
                 _help
@@ -99,7 +152,7 @@ updateFile dot-vim-colors-aurora.vim            ~/.vim/colors/aurora.vim
 
 if [[ $_OUTOFDATE -eq 0 ]];
 then
-        printf "All files are up-to-date.\n"
+        printf "All files are up-to-date with masters.\n"
 fi
 
 exit 0
